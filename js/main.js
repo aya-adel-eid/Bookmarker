35a422e99c9eb8fe4d9fb51041c53622180b0ccd
Aya Adel <ayaadelyahoo66@gmail.com>
2025-08-29 23:50:51 +0300

    add new method check if name exists &url or not

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@ var urlInput = document.getElementById("siteURL");
 var dataBody = document.getElementById("dataBody");
 var invalidName = document.getElementById("feedName");
 var invalidURL = document.getElementById("feedURL");
-var submi=document.getElementById("sub");
+var submi = document.getElementById("sub");
+var checkURL=document.getElementById("checkURL");
+var checkName=document.getElementById("checkName");
+// =========
+var headd=document.getElementById("header")
 var uRLregex =
   /^((http|https):\/\/)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}([\/a-zA-Z0-9.?:@_=#&-]*)?$/i;
 var nameRegex = /^[a-zA-Z0-9]{3,20}$/;
 
 var urlList;
-if(JSON.parse(localStorage.getItem("Sites"))!==null){
-  urlList=JSON.parse(localStorage.getItem("Sites"));
+if (JSON.parse(localStorage.getItem("Sites")) !== null) {
+  urlList = JSON.parse(localStorage.getItem("Sites"));
   displayYourFavSit(urlList);
+} else {
+  urlList = [];
 }
-else{
-  urlList=[];
-}
+
 function validtateURL(uRL) {
   var clearURL = uRL.trim();
   urlInput.classList.remove("myinput", "myinputt", "myinputs");
@@ -46,13 +50,11 @@ function validateName(name) {
     if (clearName == "") {
       nameInput.classList.add("myinput");
       invalidName.classList.replace("d-block", "d-none");
-    }
-    else{
-
+    } else {
       nameInput.classList.add("myinputs");
       invalidName.classList.replace("d-none", "d-block");
     }
-   
+
     return false;
   }
 }
@@ -63,14 +65,15 @@ function addURL() {
   };
 
   if (validateName(uRL.name) && validtateURL(uRL.sitURl)) {
-    urlList.push(uRL);
-    localStorage.setItem("Sites",JSON.stringify(urlList));
-    displayYourFavSit(urlList);
-    
-    clear();
-  } 
- 
+    if(!checkRepeateName(urlList,uRL.name)&&!checkRepeateURL(urlList,uRL.sitURl)){
 
+      urlList.push(uRL);
+      localStorage.setItem("Sites", JSON.stringify(urlList));
+      displayYourFavSit(urlList);
+  
+      clear();
+    }
+  }
 }
 function clear() {
   nameInput.value = "";
@@ -90,15 +93,48 @@ function displayYourFavSit(arr) {
     <tr>
                                 <td scope="row">${i + 1}</td>
                                 <td>${arr[i].name}</td>
-                                <td><button class="vist rounded-2"><span class="pe-1"><i class="fa-regular fa-eye"></i></span><a href="${arr[i].sitURl}" class="text-decoration-none" target="_blank"><span class="my-sp"">Visit</span> </a></button></td>
+                                <td><button class="vist rounded-2"><span class="pe-1"><i class="fa-regular fa-eye"></i></span><a href="${
+                                  arr[i].sitURl
+                                }" class="text-decoration-none" target="_blank"><span class="my-sp"">Visit</span> </a></button></td>
                                 <td><button class="del rounded-2" onclick="deleteYoueSite(${i})"><span class="pe-1"><i class="fa-solid fa-trash"></i></span> <span class=" my-sp">Delete</span></button></td>
                             </tr>
     `;
   }
-  dataBody.innerHTML=containeYorSite;
+  dataBody.innerHTML = containeYorSite;
 }
-function deleteYoueSite(index){
-  urlList.splice(index,1);
-  localStorage.setItem("Sites",JSON.stringify(urlList));
-  displayYourFavSit(urlList)
+function deleteYoueSite(index) {
+  urlList.splice(index, 1);
+  localStorage.setItem("Sites", JSON.stringify(urlList));
+  displayYourFavSit(urlList);
 }
+
+
+function checkRepeateName(arr,name ){
+  
+ for(var i=0;i<arr.length;i++){
+   if(arr[i].name===name){
+    checkName.innerHTML=`This  name site already exists at index ${i}`;
+    return true
+    } 
+ }
+ checkName.innerHTML=``;
+ return false
+}
+function checkRepeateURL(arr,url ){
+  
+ for(var i=0;i<arr.length;i++){
+   if(arr[i].sitURl===url){
+    checkURL.innerHTML=`This site already exists at index ${i}`;
+    return true
+    } 
+ }
+ checkURL.innerHTML=``;
+ return false
+}
+nameInput.addEventListener("input", function () {
+  checkRepeateName(urlList, nameInput.value);
+});
+urlInput.addEventListener("input", function () {
+  checkRepeateURL(urlList, urlInput.value);
+});
+ 
